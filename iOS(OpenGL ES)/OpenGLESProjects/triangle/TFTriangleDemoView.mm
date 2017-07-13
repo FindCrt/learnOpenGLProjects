310e8ea4c0436c4a08e0e64e65673e9df0b7e1b9
wei shi <weishi@weideMacBook-Pro.local>
2017-07-13 17:53:01 +0800

    Triangle

--- iOS(OpenGL ES)/OpenGLESProjects/triangle/TFTriangleDemoView.mm
+++ iOS(OpenGL ES)/OpenGLESProjects/triangle/TFTriangleDemoView.mm
@@ -8,15 +8,105 @@
 
 #import "TFTriangleDemoView.h"
 #import <OpenGLES/ES3/gl.h>
+#import <iostream>
 
 @interface TFTriangleDemoView (){
-    
+    GLuint program;
     GLuint VAO;
 }
 
 @end
 
 @implementation TFTriangleDemoView
+
+const GLchar *vertexShaderSource =
+"#version 300 es                            \n\
+precision mediump float;                    \n\
+layout (location = 0) in vec3 position;     \n\
+void main(){                                \n\
+gl_Position = vec4(position, 1.0f);         \n\
+}                                           \n\
+";
+const GLchar *fragmentShaderSource =
+"#version 300 es                            \n\
+out mediump vec4  color;                    \n\
+void main(){                                \n\
+color = vec4(1.0f, 0.0f, 0.0f, 1.0f);       \n\
+}                                           \n\
+";
+
+-(int)loadShadersAndLinkProgram{
+    
+    //load and compile shaders
+    
+    //vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, 0);
+    glCompileShader(vertexShader);
+    
+    GLint succeed;
+    GLchar infoLog[256];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &succeed);
+    if (!succeed) {
+        glGetShaderInfoLog(vertexShader, sizeof(infoLog),NULL, infoLog);
+        std::cout<< "compile vertex shader error: "<< infoLog << std::endl;
+        return -1;
+    }
+    
+    //fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, 0);
+    glCompileShader(fragmentShader);
+    
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &succeed);
+    if (!succeed) {
+        glGetShaderInfoLog(fragmentShader, sizeof(infoLog),NULL, infoLog);
+        std::cout<< "compile fragment shader error: "<< infoLog << std::endl;
+        return -1;
+    }
+    
+    //attach shaders and link program
+    program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    
+    glGetProgramiv(program, GL_LINK_STATUS, &succeed);
+    if (!succeed) {
+        glGetProgramInfoLog(program, sizeof(infoLog), NULL, infoLog);
+        std::cout<< "link program error: "<< infoLog << std::endl;
+        return -1;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    return 0;
+}
+
+-(void)configData{
+    
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
+    GLfloat vertices[] = {
+        -0.5f, -0.3f, 0.0f,
+        0.5f, -0.3f, 0.0f,
+        0.0f, 0.7f, 0.0f
+    };
+    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GL_FLOAT), 0);
+    glEnableVertexAttribArray(0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
 
 -(void)layoutSubviews{
     [super layoutSubviews];
@@ -26,32 +116,16 @@
 
 -(void)startRender{
     
-//    GLfloat vertices[] = {
-//        -0.5f, -0.3f, 0.0f,
-//        0.5f, -0.3f, 0.0f,
-//        0.0f, 0.6f, 0.0f
-//    };
-//    
-//    NSString *vertexPath = [[NSBundle mainBundle] pathForResource:@"triangle" ofType:@"vs"];
-//    NSString *fragmentPath = [[NSBundle mainBundle] pathForResource:@"triangle" ofType:@"fs"];
-//    _triangleProgram = new TFOPGLProgram([vertexPath UTF8String], [fragmentPath UTF8String]);
-//    
-//    
-//    glGenVertexArrays(1, &VAO);
-//    glBindVertexArray(VAO);
-//    
-//    GLuint VBO;
-//    glGenBuffers(1, &VBO);
-//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//    
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GL_FLOAT), 0);
-//    glEnableVertexAttribArray(0);
-//    
-//    glBindBuffer(GL_ARRAY_BUFFER, 0);
-//    glBindVertexArray(0);
     
     glViewport(0, 0, self.bufferSize.width, self.bufferSize.height);
+    
+    if ([self loadShadersAndLinkProgram] < 0) {
+        return;
+    }
+    
+    [self configData];
+    
+    [self rendering];
 }
 
 -(void)rendering{
@@ -62,10 +136,10 @@
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     
     
-    //_triangleProgram->use();
+    glUseProgram(program);
     
-//    glBindVertexArray(VAO);
-//    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     
     
     glBindRenderbuffer(GL_RENDERBUFFER, self.colorBuffer);
--- mac/OpenGLProjects/triangle/main.cpp
+++ mac/OpenGLProjects/triangle/main.cpp
@@ -14,6 +14,97 @@
 #include <unistd.h>
 #include <iostream>
 
+const GLchar *vertexShaderSource =
+"#version 330 core\n\
+layout (location = 0) in vec3 position;\n\
+void main(){\n\
+    gl_Position = vec4(position, 1.0f);\n\
+}\n\
+";
+const GLchar *fragmentShaderSource =
+"#version 330 core\n\
+out vec4 color;\n\
+void main(){\n\
+    color = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n\
+}\n\
+";
+
+GLuint program;
+GLuint VAO;
+
+int loadShadersAndLinkProgram(){
+    
+    //load and compile shaders
+    
+    //vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, 0);
+    glCompileShader(vertexShader);
+    
+    GLint succeed;
+    GLchar infoLog[256];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &succeed);
+    if (!succeed) {
+        glGetShaderInfoLog(vertexShader, sizeof(infoLog),NULL, infoLog);
+        std::cout<< "compile vertex shader error: "<< infoLog << std::endl;
+        return -1;
+    }
+    
+    //fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, 0);
+    glCompileShader(fragmentShader);
+    
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &succeed);
+    if (!succeed) {
+        glGetShaderInfoLog(fragmentShader, sizeof(infoLog),NULL, infoLog);
+        std::cout<< "compile fragment shader error: "<< infoLog << std::endl;
+        return -1;
+    }
+    
+    //attach shaders and link program
+    program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    
+    glGetProgramiv(program, GL_LINK_STATUS, &succeed);
+    if (!succeed) {
+        glGetProgramInfoLog(program, sizeof(infoLog), NULL, infoLog);
+        std::cout<< "link program error: "<< infoLog << std::endl;
+        return -1;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    return 0;
+}
+
+void configData(){
+    
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
+    GLfloat vertices[] = {
+        -0.5f, -0.3f, 0.0f,
+        0.5f, -0.3f, 0.0f,
+        0.0f, 0.8f, 0.0f
+    };
+    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GL_FLOAT), 0);
+    glEnableVertexAttribArray(0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
 int main(int argc, const char * argv[]) {
     
     glfwInit();
@@ -41,12 +132,20 @@ int main(int argc, const char * argv[]) {
         return -1;
     }
     
+    if (loadShadersAndLinkProgram() < 0) {
+        return -1;
+    }
+    configData();
+    
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
         
-        //Here is render commands
         glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(program);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glfwSwapBuffers(window);
     }
